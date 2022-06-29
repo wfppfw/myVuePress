1d35c38c2f8aa41548c5b5225f034b9031e026e9
wfppfw <wfppfw12138@gmail>
2022-06-29 19:46:49 +0800

    修改js路径

--- docs/.vuepress/plugins/plugin-fireworks/lib/client/components/BackToTop.js
+++ docs/.vuepress/plugins/plugin-fireworks/lib/client/components/BackToTop.js
@@ -34,9 +34,9 @@ export const Fireworks = defineComponent({
                 h('canvas',{id:'live2d',width:'200',height:'300',style:'position:fixed;left:10px;bottom:10px; z-index: 1;'}),
                 h('canvas',{class:'fireworks',style:'position: fixed; left: 0px; top: 0px; z-index: 2; pointer-events: none; '}),
                 // h('script',{},"loadlive2d('live2d','/live2d/assets/koharu.model.json');"),
-                h('script',{src:'/js/loadLive2d.js'}),
+                h('script',{src:'/myVuePress/js/loadLive2d.js'}),
                 h('script',{src:'http://cdn.bootcss.com/animejs/2.2.0/anime.min.js'}),
-                h('script',{src:'/js/fireworks.js'})
+                h('script',{src:'/myVuePress/js/fireworks.js'})
             ]
             // 'canvas',{class:'fireworks',style:'position: fixed; left: 0px; top: 0px; z-index: 1; pointer-events: none; '}
 
